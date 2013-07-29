24eceaf3cdca883ca9d42c78cf4f4e0d12bce918
刘刊 <kan@cpan.org>
2013-07-29 08:12:48 +0000

    Import of KAN/pantheon-0.08 from CPAN.
    gitpan-cpan-distribution: pantheon
    gitpan-cpan-version:      0.08
    gitpan-cpan-path:         KAN/pantheon-0.08.tar.gz
    gitpan-cpan-author:       KAN
    gitpan-cpan-maturity:     released

--- lib/pantheon.pm
+++ lib/pantheon.pm
@@ -7,13 +7,8 @@ use warnings;
 
 pantheon - A suite of cluster administration platforms
 
-=head1 VERSION
-
-Version 0.01
-
 =cut
-
-our $VERSION = '0.07';
+our $VERSION = '0.08';
 
 =head1 MODULES
 
