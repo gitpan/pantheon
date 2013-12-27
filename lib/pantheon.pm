80e5198a7fe3107830e9f00b82dd7fc45e0579ae
刘刊 <kan@cpan.org>
2013-12-27 03:24:42 +0000

    Import of KAN/pantheon-0.50 from CPAN.
    gitpan-cpan-distribution: pantheon
    gitpan-cpan-version:      0.50
    gitpan-cpan-path:         KAN/pantheon-0.50.tar.gz
    gitpan-cpan-author:       KAN
    gitpan-cpan-maturity:     released

--- lib/pantheon.pm
+++ lib/pantheon.pm
@@ -8,7 +8,7 @@ use warnings;
 pantheon - A suite of cluster administration tools and platforms
 
 =cut
-our $VERSION = '0.49';
+our $VERSION = '0.50';
 
 =head1 MODULES
 
--- pantheon/seco/lib/SECO.pm
+++
@@ -1,9 +0,0 @@
-package SECO;
-
-use strict;
-use warnings;
-
-our $DOMAIN = '.qihoo.net';
-our %USER = ( engine => 'search', index => 'cloudops' );
-
-1;
